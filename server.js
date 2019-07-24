78cab31ee52dc8264fb838d10bc2376631ba4566
Aditya Satrya <aditya.satrya@gmail.com>
2019-07-24 12:15:20 +0700

    upgrade to v3 and deploy automatically

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Cloud Run v2')
+    res.end('Hello Cloud Run v3')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
